e38ef7409ac3b0b8f88537115cf7522945c3ad8d
EwickVon <113259024+EwickVon@users.noreply.github.com>
2023-03-28 14:02:17 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+import pygame, sys # import pygame and sys
+from Level_map import *
+
+
+from pygame.locals import * # import pygame modules
+clock = pygame.time.Clock() # set up the clock
+pygame.init() # initiate pygame
+
+pygame.display.set_caption('Better minecraft') # set the window name
+
+WINDOW_SIZE = (SCREEN_WIDTH, screen_height) # set up window size
+screen = pygame.display.set_mode(WINDOW_SIZE,0,32) # initiate screen
+
+display = pygame.Surface((rescaled_width, rescaled_height))
+
+level = Level([], 'data/level', display)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == KEYDOWN:
+            if event.key == K_SPACE:
+                level.button_held()
+        if event.type == KEYUP:
+            if event.key == K_SPACE:
+                level.button_released()
+
+    display.fill('red')  # fill the screen with black
+    level.run()
+
+    screen.blit(pygame.transform.scale(display,WINDOW_SIZE),(0,0))
+    pygame.display.update() # update the screen
+    clock.tick(60) # update the clock
